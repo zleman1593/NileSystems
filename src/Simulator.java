873518b5ca6e66bf57e5da957a05aa23b31b6f79
Zackery Leman <zleman1593@gmail.com>
2015-03-23 13:32:53 -0400

    updated tests

--- src/Simulator.java
+++ src/Simulator.java
@@ -28,63 +28,33 @@ public class Simulator {
 				
 			}
 		};
-
-
-		//Start 2 Thread Test
-		System.out.println("Start 2 Thread Test");
-		Thread thr1 = new Thread(r1);
-		Thread thr2 = new Thread(r1);
-
-		
-		thr1.start();
-		thr2.start();
-	
-		
-		try {
-			thr1.join();
-			thr2.join();
-
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		//Start 4 Thread Test
-		System.out.println("Start 4 Thread Test");
-		Thread thrA = new Thread(r1);
-		Thread thrB = new Thread(r1);
-		Thread thrC = new Thread(r1);
-		Thread thrD = new Thread(r1);
-		
-		thrA.start();
-		thrB.start();
-		thrC.start();
-		thrD.start();
-		
-		try {
-			thrA.join();
-			thrB.join();
-			thrC.join();
-			thrD.join();
-
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 	
 	
 	//Start many Thread Test
 	System.out.println("Start Many Thread Test");
-	int threadCount = 50;
+	int threadCount = 30;
 	Thread[] threads = new Thread[threadCount];
 	for(int i = 0; i < threadCount; i++){
 		threads[i] = new Thread(r1);
 	}
-	
 	for(int i = 0; i < threadCount; i++){
 		threads[i].start();
 	}
 	
 	}
-	
+	// Results
+	// Number of clients (buy time, search time)
+	// 1   (5,1)
+	// 2   (5,2)
+	// 4   (8,3)
+	// 10  (8,6)
+	// 20  (15,11)
+	// 30	(23,17)
+	// 40  (30,21)
+	// 50  (32,21)
+	// 100	(63,41)
+	// 500	(300,200)
+	//Time is in ms
 
 	/*
 	 * // Third client Client third = new Client("localhost", port);
@@ -110,7 +80,7 @@ public class Simulator {
 		Client second = new Client(ip, port);
 		long startTime = System.currentTimeMillis();
 		for (int i = 0; i < iterations; i++) {
-			second.lookUpById("57471");
+			second.searchByTopic("college life");
 		}
 		long executionTime = System.currentTimeMillis() - startTime;
 		long averageTime = executionTime / iterations;
